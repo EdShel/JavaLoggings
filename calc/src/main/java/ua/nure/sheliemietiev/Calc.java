903aee2dda6864fa442f0bee869e3947b88f16a2
EdShel <eduardchelemetjev@gmail.com>
2021-12-21 21:28:19 +0200

    Task 1.2

--- calc/src/main/java/ua/nure/sheliemietiev/Calc.java
+++ calc/src/main/java/ua/nure/sheliemietiev/Calc.java
@@ -1,13 +1,20 @@
 package ua.nure.sheliemietiev;
 
-/**
- * Hello world!
- *
- */
 public class Calc 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static int add(int x, int y) {
+		return x + y;
+	}
+	
+	public static int sub(int x, int y) {
+		return x - y;
+	}
+	
+	public static int div(int x, int y) {
+		return x / y;
+	}
+	
+	public static int mul(int x, int y) {
+		return x * y;
+	}
 }
