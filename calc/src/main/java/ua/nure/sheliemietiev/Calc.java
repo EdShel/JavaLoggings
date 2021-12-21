33bf8101c1606e7270af65691cd9831cd12c4188
EdShel <eduardchelemetjev@gmail.com>
2021-12-21 22:34:55 +0200

    Task 1.7

--- calc/src/main/java/ua/nure/sheliemietiev/Calc.java
+++ calc/src/main/java/ua/nure/sheliemietiev/Calc.java
@@ -1,20 +1,40 @@
 package ua.nure.sheliemietiev;
 
-public class Calc 
-{
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Calc {
+	private static final Logger log = Logger.getLogger(Calc.class.getName());
+
 	public static int add(int x, int y) {
+		log.info("add");
+		log.log(Level.FINEST, "x = {0}", x);
+		log.log(Level.FINEST, "y = {0}", y);
+
 		return x + y;
 	}
-	
+
 	public static int sub(int x, int y) {
+		log.config("sub");
+		log.log(Level.FINEST, "x = {0}", x);
+		log.log(Level.FINEST, "y = {0}", y);
+
 		return x - y;
 	}
-	
+
 	public static int div(int x, int y) {
+		log.config("div");
+		log.log(Level.FINEST, "x = {0}", x);
+		log.log(Level.FINEST, "y = {0}", y);
+
 		return x / y;
 	}
-	
+
 	public static int mul(int x, int y) {
+		log.config("mul");
+		log.log(Level.FINEST, "x = {0}", x);
+		log.log(Level.FINEST, "y = {0}", y);
+
 		return x * y;
 	}
 }
--- calc/src/main/java/ua/nure/sheliemietiev/Demo.java
+++ calc/src/main/java/ua/nure/sheliemietiev/Demo.java
@@ -1,10 +1,16 @@
 package ua.nure.sheliemietiev;
 
+import java.util.logging.Logger;
+
 public class Demo {
+	private static final Logger log = Logger.getLogger(Demo.class.getName());
+
 	public static void main(String[] args) {
+		log.info("main");
+		
 		final int x = 100;
 		final int y = 5;
-		
+
 		System.out.printf("x = %d, y = %d%n", x, y);
 		System.out.printf("Calc.add %d%n", Calc.add(x, y));
 		System.out.printf("Calc.sub %d%n", Calc.sub(x, y));
